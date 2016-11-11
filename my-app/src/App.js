efc40e67729a7336402cac544682be59c32da2e4
Nofirt <trifon.j.trifonov@gmail.com>
2016-11-11 13:29:32 +0200

    Some changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,16 +6,55 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  <GetInfo />
+  <ul><OutputInfo /></ul>
       </div>
     );
   }
 }
 
+class ToDoList extends Component {
+  constructor(){
+    super();
+    this.state={todos:[{name:'list',isCompleted:false}]}
+  }
+
+  render() {
+    return(
+      'hi'
+    )
+  }
+}
+
+class GetInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value:''};
+    this.addToDoItem = this.addToDoItem.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+  addToDoItem(event){
+    if(event.key === 'Enter'){
+      console.log(this.state.value);
+      this.props.save(this.state.value);
+    }
+  }
+  render(){
+    return(
+      <input type='text' value={this.state.value} onKeyPress={this.addToDoItem} onChange={this.handleChange} placeholder='What you want to add?' />
+    )
+  }
+}
+
+class OutputInfo extends Component {
+  constructor(){}
+  render(){
+    return(
+      <li>{}</li>
+    )
+  }
+}
 export default App;
